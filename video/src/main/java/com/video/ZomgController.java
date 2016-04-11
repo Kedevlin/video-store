2b39c0e8ed58399728ffbdb85d39359ce6b25e23
Kelly <kelly_devlin@alumni.brown.edu>
2016-04-11 16:04:30 -0700

    Zomg controller responds with json data including 'it works'

--- video/src/main/java/com/video/ZomgController.java
+++ video/src/main/java/com/video/ZomgController.java
@@ -2,18 +2,16 @@ package com.video;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZomgController {
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/zomg")
-    public Zomg zomg(@RequestParam(value="name", defaultValue="it works!") String name) {
+    public Zomg zomg() {
         return new Zomg(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format("it works!"));
     }
 
 }
